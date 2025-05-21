88f33daf0a220fa221ed38fa31a8c6f084bba301
Adams Souza <adamsdossantos@yahoo.com.br>
2025-05-21 16:50:57 -0300

    main adjustment

--- project_api/main.py
+++ project_api/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Depends
 from route import aplicativo
+import uvicorn
+import os
 
 app = FastAPI()
 
@@ -9,3 +11,6 @@ app.include_router(aplicativo.router)
 async def root():
     return {"message": "Este é o API do Tech Challenge 4 da FIAP"}
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
